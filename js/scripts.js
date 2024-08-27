f4bc4370adb222f3705e51f08374c4fe1b56c7a2
Thiago Ferrer <thi-ferrer@live.com>
2024-08-27 15:57:29 -0300

    atualização de metas.html

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,39 @@ document.querySelectorAll('.bar').forEach(function(bar) {
     bar.style.height = height + '%';
 });
 
+
+// grafico metas.html
+function createHalfDoughnutChart(ctx, planejado, realizado) {
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [realizado, planejado - realizado],
+                backgroundColor: ['#ea1d2c', '#CCCCCC'],
+                hoverBackgroundColor: ['#FF6384', '#EEEEEE'],
+                borderWidth: 0
+            }],
+            labels: ['Realizado', 'Planejado'],
+        },
+        options: {
+            circumference: 180,
+            rotation: 270,
+            cutout: '70%',
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    createHalfDoughnutChart(document.getElementById('chartDespesas'), 30, 10);
+    createHalfDoughnutChart(document.getElementById('chartSaldo'), 70, 50);
+    createHalfDoughnutChart(document.getElementById('chartClientes'), 90, 85);
+    createHalfDoughnutChart(document.getElementById('chartReceitas'), 80, 60);
+    createHalfDoughnutChart(document.getElementById('chartVendas'), 80, 30);
+});
+
