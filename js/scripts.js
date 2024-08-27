b7bd39d0a433ea17ea85046890b759669af7d325
Thiago Ferrer <thi-ferrer@live.com>
2024-08-26 23:05:25 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
+// grafico
+document.querySelectorAll('.bar').forEach(function(bar) {
+    var value = bar.getAttribute('data-value');
+    var maxValue = 6000; // Ajuste conforme necessário
+    var height = (value / maxValue) * 100;
+    bar.style.height = height + '%';
+});
+
+
+
+
+
